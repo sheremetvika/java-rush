5515186e5043f3e5fbfceb14eff743ce7b041708
sheremetvika <sheremet24viktoriia@gmail.com>
2023-05-03 23:02:16 -0400

    task-multiarrays-resolved

--- src/com/javarush/task/pro/task05/task0511/Solution.java
+++ src/com/javarush/task/pro/task05/task0511/Solution.java
@@ -11,5 +11,19 @@ public class Solution {
 
     public static void main(String[] args) {
         //напишите тут ваш код fgdfgdfgdfdfgfdgfdgdfg
+        Scanner console = new Scanner(System.in);
+        int n = console.nextInt();
+        multiArray = new int[n][];
+        int r = 0;
+        for (int i = 0; i < n; i++) {
+            r = console.nextInt();
+            multiArray[i] = new int[r];
+            //System.out.println(multiArray[i][0]);
+        }
+
+        //for (int i = 0; i < multiArray.length; i++) {
+        //System.out.println(multiArray[i][]);
+        //}
+
     }
 }
